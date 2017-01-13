0ac938967a48d7187af0a7629c44d87a625cc414
Boatiful <dutinfo.larr@gmail.com>
2017-01-13 08:41:29 +0100

    add video

--- js/contenu.js
+++ js/contenu.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
-	resizeCard(3/4);
-      $('.parallax').parallax();
+// $(document).ready(function(){
+// 	resizeCard(3/4);
+//       $('.parallax').parallax();
       
-      $(window).on('resize', function() {
-      	resizeCard(3/4);
-      });
+//       $(window).on('resize', function() {
+//       	resizeCard(3/4);
+//       });
 
-});
+// });
 
 
-function resizeCard(radio) {
-	var width = $('.card-about').css('width');
-    var widthCalc = width.substr(0, width.length - 2);
-    $('.card-about').css('height', radio*widthCalc);
-    var height = $('.card-about').css('height');
-}
+// // function resizeCard(radio) {
+// // 	var width = $('.card-about').css('width');
+// //     var widthCalc = width.substr(0, width.length - 2);
+// //     $('.card-about').css('height', radio*widthCalc);
+// //     var height = $('.card-about').css('height');
+// // }
